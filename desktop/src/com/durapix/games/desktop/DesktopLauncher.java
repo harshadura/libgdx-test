0985d97d3d20068c0594a509e878d272a7ada7c0
Harsha Siriwardena <Harsha@Prabodhas-Mac-mini.local>
2014-08-12 20:34:29 +0530

    iniital commit

---
+++ desktop/src/com/durapix/games/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.durapix.games.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.durapix.games.Durevo;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Durevo(), config);
+	}
+}
